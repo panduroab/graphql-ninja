1d573729ab808c4d9d5079280f58830abc4c37aa
Abraham <panduroab@gmail.com>
2020-08-29 18:46:41 -0500

    createUser and deleteUser Mutations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { ApolloServer, gql } = require('apollo-server-express');
-const { cars, users } = require('./data');
+let { cars, users } = require('./data');
 const me = users[0];
 
 //GraphQL is agnostic about the storage data
@@ -28,6 +28,11 @@ const typeDefs = gql`
     cars: [Car]
     car(id: Int!): Car
   }
+
+  type Mutation {
+    createUser(id: Int!, name: String!): User!
+    deleteUser(id: Int!): Boolean!
+  }
 `;
 
 //The resolver is the controller or model that resolves the query defined in the schema
@@ -38,6 +43,27 @@ const resolvers = {
     cars: () => cars,
     car: (parent, { id }) => cars.filter(car => car.id === id)[0],
     me: () => me
+  },
+  Mutation: {
+    createUser: (parent, { id, name }) => {
+      const user = {
+        id,
+        name
+      };
+      users.push(user);
+      return user;
+    },
+    deleteUser: (parent, { id }) => {
+      let found = false;
+      users = users.filter(user => {
+        if (user.id === id) {
+          found = true;
+        } else {
+          return users;
+        }
+      });
+      return found;
+    }
   },
   User: {
     cars: parent => cars.filter(car => car.ownedBy === parent.id)
@@ -51,6 +77,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers
 });
+
 server.applyMiddleware({ app });
 
 app.listen(3000, () => console.info('Apollo GraphQL server is running on port localhost:3000/graphql'));
--- data.js
+++ data.js
@@ -33,4 +33,3 @@ module.exports = {
   users,
   cars
 };
-
