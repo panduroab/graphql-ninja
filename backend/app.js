a8753700c06ceedc8ea24a78cb4f4c4332e90005
Abraham <panduroab@gmail.com>
2020-09-06 20:50:35 -0500

    Create car and relate it with a User

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
@@ -8,16 +9,29 @@ const models = require('./models');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
+const getLoggedInUser = req => {
+  const token = req.headers['x-auth-token'];
+  if (token) {
+    try {
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      return verified;
+    } catch (error) {
+      throw new Error('Session expired');
+    }
+  }
+};
+
 //const me = models.users[0];
 //GraphQL is agnostic about the storage data
 //Schema definition for GraphQL Queries and Mutations
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
+  context: ({ req }) => ({
     models,
-    secret: process.env.JWT_SECRET
-  }
+    secret: process.env.JWT_SECRET,
+    me: getLoggedInUser(req)
+  })
 });
 
 server.applyMiddleware({ app });
